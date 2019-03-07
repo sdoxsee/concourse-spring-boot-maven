446f11755377e0adc91dccd007f2cfaae641988e
Stephen Doxsee <stephen@doxsee.org>
2019-03-07 15:00:07 -0500

    add cache

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -2,5 +2,15 @@
 
 set -e -u -x
 
+export ROOT_FOLDER=$( pwd )
+export REPO=repo
+
+M2_HOME="${HOME}/.m2"
+M2_CACHE="${ROOT_FOLDER}/maven"
+
+echo "Generating symbolic links for caches"
+
+[[ -d "${M2_CACHE}" && ! -d "${M2_HOME}" ]] && ln -s "${M2_CACHE}" "${M2_HOME}"
+
 cd source-code/
 ./mvnw package
